fe68a920ea6562bd52573bda72fa22d4d1a687f9
sadighasanzade <sadiqhesensr@gmail.com>
2023-09-06 01:55:46 +0400

    project created and connected to postgresql database

---
+++ src/main/java/com/example/playground/user/User.java
@@ -0,0 +1,80 @@
+package com.example.playground.user;
+
+import java.time.LocalDate;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+    private LocalDate dob;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, int age, LocalDate dob) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dob = dob;
+    }
+
+    public User(String name, String email, int age, LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dob = dob;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", dob=" + dob +
+                '}';
+    }
+}
